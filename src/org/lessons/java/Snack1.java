56fee31d1f259a16ec26978f782116020d181381
Marco 9726 <marcosarra9@gmail.com>
2023-05-05 10:47:09 +0200

    snack 1

---
+++ src/org/lessons/java/Snack1.java
@@ -0,0 +1,18 @@
+package org.lessons.java;
+
+import java.util.Scanner;
+
+public class Snack1 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Inserire un numero: ");
+		
+		int num = in.nextInt();
+		
+		if ( num % 2 == 0) {
+			System.out.println(num);
+		} else {
+			System.out.println(++num);
+		}
+	}
+}
